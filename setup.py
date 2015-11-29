b204852770b7a7823d2016a0b8e5c737a19196a5
Yaroslav D <yaroslav.dashivets@gmail.com>
2015-11-28 22:47:01 -0500

    - Changed license - Fixed setup.py

--- setup.py
+++ setup.py
@@ -9,25 +9,19 @@ setup(name='django-email-processing',
       long_description="",
       classifiers=[
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "License :: OSI Approved :: BSD License",
         ],
-      keywords='',
+      author = 'Yaroslav D.',
       author_email='iyarslv@gmail.com',
       url='',
-      license='Apache',
-      packages=find_packages('email_processing'),
-      package_dir={'':'email_processing'},
+      packages=['email_processing'],
       include_package_data=True,
-      zip_safe=False,
-      extras_require = dict(
-        test = []
-        ),
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
       # -*- Entry points: -*-
-      """,
-      dependency_links = [],
+      """
       )
